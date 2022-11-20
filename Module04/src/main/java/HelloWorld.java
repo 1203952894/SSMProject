fe880987437cd0440ddb8209d4353874128325dc
Cola777 <116149638+nowByMySelf@users.noreply.github.com>
2022-11-20 16:19:01 +0800

    Module05

---
+++ Module04/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @ Name: HelloWorld
+ * @ Author: Cola
+ * @ Time: 2022/11/20 16:18
+ * @ Description: TODO
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
